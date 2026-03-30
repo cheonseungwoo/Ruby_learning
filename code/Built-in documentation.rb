2da273b5e699017ada54b583007a1ac2c0b87382
Seungwoo Cheon <chdnwkfj@gmail.com>
2026-03-30 11:07:05 +0900

    sss

---
+++ code/Built-in documentation.rb
@@ -0,0 +1,8 @@
+=begin
+  Built-in documentation
+  test test
+  아아아아아아아
+  puts "test"
+  여러줄 주석
+=end
+puts "Built-in documentation"
--- code/Comment.rb
+++ code/Comment.rb
@@ -2,12 +2,3 @@ puts "test"
 # this is a comment
 puts 1+1
 puts 18*4 # this is a comment too
-
-=begin
-  Built-in documentation
-  test test
-  아아아아아아아
-  puts "test"
-  여러줄 주석
-=end
-puts "Built-in documentation"
