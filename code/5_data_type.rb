43dd1ea1bd554f2e04e5f6bcde6cd7b0b4ae9cd1
Seungwoo Cheon <chdnwkfj@gmail.com>
2026-04-27 20:13:32 +0900

    5_2

---
+++ code/5_data_type.rb
@@ -0,0 +1,7 @@
+#Numeric : 정수(Integer)와 실수(Float)를 포함합니다.
+#String : 문자열 (#{}를 쓰는 보간법은 큰따옴표("")에서만 가능합니다.)
+#Symbol : 이름표 (수정 불가능한 고유 이름입니다. 메모리 효율이 좋습니다.)
+#Boolean : 블리언 true, false (모두 소문자)
+#NilClass : 비어있음 (값이 없음)
+#Array : 배열
+#Hash : 해시(키(Key)와 값(Value)의 쌍으로 이루어진 사전입니다.)
